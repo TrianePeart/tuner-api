bc1c131478517ccf747d513f8a850e0d94c41493
Sara Perkaj <saraperkaj@pursuit.org>
2022-01-31 14:38:34 -0500

    added some code Triane was working on.

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,11 +1,32 @@
-const express = require('express');
+const express = require("express");
 const songs = express.Router();
-const {getAllSongs} = require('../queries/songs');
+const { getAllSongs, getSong, createSong } = require("../queries/songs");
 
-songs.get('/', async (req, res) => {
-    const allSongs = await getAllSongs();
-    console.log(allSongs)
-    res.status(200).json(allSongs)
+songs.get("/", async (req, res) => {
+  const allSongs = await getAllSongs();
+  console.log(allSongs);
+  res.status(200).json(allSongs);
 });
 
-module.exports = songs; 
+songs.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const song = await getSong(id);
+  if (song) {
+    res.status(200).json(song);
+  } else {
+    res.status(404).json({ Error: `Page not found!` });
+  }
+});
+
+songs.post("/", async (req, res) => {
+  const newTrack = req.body;
+  const { body } = req;
+  const { name, artist, album, time } = body;
+  if (name || artist || album || time) {
+    res.status(200).json(newTrack);
+  } else {
+    res.status(400).json({ error: `Complete all fields` });
+  }
+});
+
+module.exports = songs;
--- queries/songs.js
+++ queries/songs.js
@@ -1,12 +1,34 @@
-const db = require('../dbConfig.js')
+const db = require("../dbConfig.js");
 
-const getAllSongs = async () =>{
-    try{
-      const allSongs = await db.any('SELECT * FROM songs');  
-      return allSongs
-    } catch (error){
-        console.log(error);
-    }
+const getAllSongs = async () => {
+  try {
+    const allSongs = await db.any("SELECT * FROM songs");
+    return allSongs;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-module.exports = {getAllSongs} ;
+const getSong = async (id) => {
+  try {
+    const song = await db.one(`SELECT * FROM songs WHERE id`, id);
+    return song;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const createSong = async (newSong) => {
+  const { name, artist, album, time, is_favorite } = newSong;
+  try {
+    const newTrack = await db.one(
+      "INSERT INTO songs(name, artist, album, time, is_favor)",
+      [name, artist, album, time, is_favorite]
+    );
+    return newTrack;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { getAllSongs, getSong, createSong };
